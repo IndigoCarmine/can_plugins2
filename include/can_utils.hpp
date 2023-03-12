48ff1dea17eb7137cc813992e81e6740d23d32d8
IndigoCarmine <tamanisikaminai@gmail.com>
2023-03-13 02:56:14 +0900

    fixed shared lib settings.

--- include/can_utils.hpp
+++ include/can_utils.hpp
@@ -1,32 +1,33 @@
-#include <boost/array.hpp>
+#include <array>
 #include <can_plugins2/msg/frame.hpp>
 #define CAN_MTU 8
 
 
 template <typename T>
-static void can_unpack(const boost::array<uint8_t, CAN_MTU> &buf, T &data)
+static void can_unpack(const std::array<uint8_t, CAN_MTU> &buf, T &data)
 {
   memcpy(&data, buf.data(), sizeof(T));
 }
 
 template<typename T>
-static void can_pack(boost::array<uint8_t, CAN_MTU> &buf, const T data)
+static void can_pack(std::array<uint8_t, CAN_MTU> &buf, const T data)
 {
   memcpy(buf.data(), &data, sizeof(T));
 }
 
 template<typename T>
-static can_plugins2::msg::Frame get_frame(const uint16_t id, const T data)
+static std::unique_ptr<can_plugins2::msg::Frame> get_frame(const uint16_t id, const T data)
 {
-  can_plugins::Frame frame;
-  frame.id = id;
-  frame.is_rtr = false;
-  frame.is_extended = false;
-  frame.is_error = false;
+  std::unique_ptr<can_plugins2::msg::Frame> frame;
+  frame->id = id;
+  frame->is_rtr = false;
+  frame->is_extended = false;
+  frame->is_error = false;
 
-  frame.dlc = sizeof(T);
+  frame->dlc = sizeof(T);
+  frame->data.fill(0);
 
-  can_pack<T>(frame.data, data);
+  can_pack<T>(frame->data, data);
 
   return frame;
 }
